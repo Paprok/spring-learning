728281270027f0f63f3e5af14c0df14bff7b52ff
Paprok <rafal.becla@gmail.com>
2019-03-07 15:45:34 +0100

    working hibernate and controller

---
+++ src/main/java/com/codecool/data/NoteController.java
@@ -0,0 +1,35 @@
+package com.codecool.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NoteController {
+
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    public NoteController(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+
+    // Get a Single Note
+
+    // Update a Note
+
+    // Delete a Note
+}
